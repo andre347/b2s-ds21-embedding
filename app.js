898917955bc40bef255d4f6cc698a7ee68d0ef3d
Andre <andre.devries@theinformationlab.co.uk>
2022-02-09 15:13:48 +0000

    Add filter logic
    Add filter logic pre-loading of viz and after viz loaded

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const url =
 const vizContainer = document.getElementById("vizContainer");
 const vizOptions = {
   device: "desktop",
+  Region: ["North", "South"],
 };
 let viz;
 // 1. Grab button from HTML
@@ -39,3 +40,28 @@ powerPointButton.addEventListener("click", exportPowerPoint);
 document.getElementById("download").addEventListener("click", function () {
   viz.showDownloadDialog();
 });
+
+// function to grab the filter values and filter the viz
+function getRangeValues() {
+  // 1. Get the values from the input boxes
+  const minValue = document.getElementById("minValue").value;
+  const maxValue = document.getElementById("maxValue").value;
+  console.log({ minValue, maxValue });
+  // get the workbook
+  const workbook = viz.getWorkbook();
+  // get active sheet - dashboard
+  const activeSheet = workbook.getActiveSheet();
+  // get all sheets in active sheet
+  const sheets = activeSheet.getWorksheets();
+  const sheetToFilter = sheets[1];
+  console.log(sheetToFilter);
+  sheetToFilter
+    .applyRangeFilterAsync("SUM(Sales)", {
+      min: minValue,
+      max: maxValue,
+    })
+    .then(alert("FILTER APPLIED!!!!!"));
+}
+
+// filter the dashboard for sales
+document.getElementById("filterBtn").addEventListener("click", getRangeValues);
